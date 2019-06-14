ba00147670e21455e6ad7efe5e477c874534a27a
captivating-form-0787 <avertrees447@gmail.com>
2019-06-14 14:52:31 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
+
 def square_array(array)
   # your code here
+  squared = Array.new(array.length)
+  i = 0
+  while i < array.length do
+    squared[i] = array[i]**2
+    i+=1
+  end
+  return squared
 end
